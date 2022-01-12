8d5516427faeada43fb40b0e0c109993352f3c96
Samuel Ogu <hello@samuelogu.com>
2022-01-12 15:18:51 +0100

    home page structuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RootedComponent } from './rooted/rooted';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent, RootedComponent, NavbarComponent
+    AppComponent, RootedComponent, NavbarComponent, HeroComponent, PostsComponent, SidebarComponent
   ],
   imports: [
     BrowserModule,
